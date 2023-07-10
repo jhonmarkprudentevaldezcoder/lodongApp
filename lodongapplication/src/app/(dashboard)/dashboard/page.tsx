3d42ab26995bea27b7121c368d67ba558d63a7b4
JHON MARK PRUDENTE VALDEZ <jhonmarkprudentevaldezcoder@gmail.com>
2023-07-10 17:02:12 +0800

    login form

--- lodongapplication/src/app/(dashboard)/dashboard/page.tsx
+++ lodongapplication/src/app/(dashboard)/dashboard/page.tsx
@@ -1,15 +1,13 @@
+import Nav from "@/components/Nav/Nav";
+
 export default function Dashboard() {
   return (
     <div className="flex flex-col md:flex-row">
       {/* side navigation  */}
-      <aside className="w-100 md:w-64 bg-gray-200  sm:block md:w-1/8">
-        NAV
-      </aside>
-
-      {/* Main content */}
-      <main className="flex-1">Your main content goes here</main>
-
-      <aside className="w-100 md:w-64 bg-gray-200  md:w-1/4">right</aside>
+      <Nav />
+      {/* detail */}
+      <main>nmaind i v</main>
+      <aside className="bg-gray-200">right</aside>
     </div>
   );
 }
