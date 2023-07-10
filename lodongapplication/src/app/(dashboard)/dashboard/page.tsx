f4cd74cceb550f39dffd0b37f5162b6ef2e38f8e
JHON MARK PRUDENTE VALDEZ <jhonmarkprudentevaldezcoder@gmail.com>
2023-07-10 15:02:07 +0800

    uodate

--- lodongapplication/src/app/(dashboard)/dashboard/page.tsx
+++ lodongapplication/src/app/(dashboard)/dashboard/page.tsx
@@ -1,3 +1,15 @@
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="flex flex-col md:flex-row">
+      {/* side navigation  */}
+      <aside className="w-100 md:w-64 bg-gray-200  sm:block md:w-1/8">
+        NAV
+      </aside>
+
+      {/* Main content */}
+      <main className="flex-1">Your main content goes here</main>
+
+      <aside className="w-100 md:w-64 bg-gray-200  md:w-1/4">right</aside>
+    </div>
+  );
 }
