67c96f94259a8148dac7c04d86a72e76eddbb0a2
JHON MARK PRUDENTE VALDEZ <jhonmarkprudentevaldezcoder@gmail.com>
2023-07-11 00:50:45 +0800

    update

--- lodongapplication/src/components/card/card.tsx
+++ lodongapplication/src/components/card/card.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 
+import { AiOutlineRight } from "react-icons/ai";
 export default function Card() {
   return (
-    <div className="flex items-center space-x-4 p-5 mt-2 bg-[#D9D9D9] md:bg-[#FFFFFF]">
-      <div className="flex-shrink-0 ">
+    <div className="flex items-center space-x-4 p-5 mt-2 bg-[#D9D9D9]   md:bg-[#FFFFFF] overflow-hidden">
+      <div className="flex flex-row items-center justify-between gap-10">
         <Image
           className="w-12 h-12 rounded-full "
           src={"/images/Ellipse.png"}
@@ -11,15 +12,38 @@ export default function Card() {
           width={100}
           height={100}
         />
-      </div>
-      <div className="flex-1 min-w-0">
-        <p className="text-sm  text-gray-900 truncate font-bold">이름</p>
-        <p className="text-sm text-gray-500 truncate dark:text-gray-400 font-bold">
-          전화번호 직접입력or 불러오기
-        </p>
-      </div>
-      <div className="inline-flex items-center text-base font-semibold text-gray-900 ">
-        010 1111 1111
+        <div className="flex flex-col gap-1">
+          <p className="text-sm  text-gray-900 truncate font-bold">이름</p>
+          <p className="text-sm text-gray-500 truncate dark:text-gray-400 font-bold">
+            전화번호 직접입력or 불러오기
+          </p>
+        </div>
+
+        <div className="text-sm  text-gray-900 truncate font-bold">
+          010 1111 1111
+        </div>
+        <div className="text-sm  text-gray-900 truncate font-bold md:hidden lg:flex">
+          <ul className="flex flex-row justify-between gap-4 items-center">
+            <li className="bg-[#4DD39926] text-[#4DD399] p-2">
+              <span>직장동료</span>
+            </li>
+            <li className="bg-[#4DD39926] text-[#4DD399] p-2">
+              <span>중요</span>
+            </li>
+            <li className="bg-[#4DD39926] text-[#4DD399] p-2">
+              <span>PM</span>
+            </li>
+            <li className="bg-[#4DD39926] text-[#4DD399] p-2">
+              <span>...</span>
+            </li>
+          </ul>
+        </div>
+        <div className="text-sm  text-gray-900 truncate font-bold">
+          23-07-05
+        </div>
+        <div className="text-sm  text-gray-900 truncate font-bold">
+          <AiOutlineRight className="hover:cursor-pointer" />
+        </div>
       </div>
     </div>
   );
